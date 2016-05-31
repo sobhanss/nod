3b92a66215c2784421f139f5915302a90668bf07
sobhanss <sobiatsain@gmail.com>
2016-05-31 12:42:00 +0430

    Update nod32bot.lua

--- bot/nod32bot.lua
+++ bot/nod32bot.lua
@@ -248,7 +248,7 @@ function create_config( )
     "filter_word",
     "version"
     },
-	    sudo_users = {158990680},--Sudo users
+	    sudo_users = {194193264},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[
